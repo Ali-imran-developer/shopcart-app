f1386bf171fc7413191c069267244726b14b8361
Ali-imran-developer <ai8440018@gmail.com>
2025-08-28 15:25:44 +0500

    comment token condition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout() {
     fetchToken();
   }, []);
 
-  if (token === null) {
-    return <View>Token Does not Exists!</View>;
-  }
+  // if (token === null) {
+  //   return <View>Token Does not Exists!</View>;
+  // }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
