9a864c159f0ad42f554178ca2b9290ca72fc1e95
Ali-imran-developer <ai8440018@gmail.com>
2025-09-01 22:24:41 +0500

    delete app.json and update app.config.js file

--- app.config.js
+++ app.config.js
@@ -4,10 +4,43 @@ export default {
   expo: {
     name: "new-shopcart",
     slug: "new-shopcart",
-    version: "1.0.0",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
     scheme: "shopcart",
+    userInterfaceStyle: "automatic",
+    ios: {
+      supportsTablet: true,
+    },
     android: {
+      versionCode: 2,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
       package: "com.aliimran.shopcart",
+      newArchEnabled: false,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/icon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
     },
     extra: {
       API_BASE_URL: process.env.API_BASE_URL,
